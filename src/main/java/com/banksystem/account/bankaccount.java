7b16ef5c5a6d93c6a238208486db32d2d7710f83
Busra <busrkarasu@gmail.com>
2026-03-19 01:49:05 +0300

    Phase1-W1: Encapsulation - BankAccount class

---
+++ src/main/java/com/banksystem/account/bankaccount.java
@@ -0,0 +1,24 @@
+package com.banksystem.account;
+
+    public class bankaccount {
+        private double balance;
+        private String accountNumber;
+
+        public bankaccount(String accountNumber) {
+            this.accountNumber = accountNumber;
+            this.balance = 0.0;
+        }
+
+        public void deposit(double amount) {
+            if (amount <= 0) throw new IllegalArgumentException("Invalid amount");
+            this.balance += amount;
+        }
+
+        public void withdraw(double amount) {
+            if (amount <= 0) throw new IllegalArgumentException("Invalid amount");
+            if (amount > balance) throw new IllegalArgumentException("Insufficient balance");
+            this.balance -= amount;
+        }
+        public double getBalance() { return balance; }
+        public String getAccountNumber() { return accountNumber; }
+    }
